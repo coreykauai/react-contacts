279408ade5e766d212cfcd935617c5407685d05f
Coreykauai <corey@student.dev-island.org>
2024-02-22 18:44:27 -1000

    setup overall layout and basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="page"></div>;
+  return (
+    <div className="page">
+      <h1>React Contacts</h1>
+      <div className="contact-card">
+        <h3>Jenny Smith</h3>
+        <p>Phone: 808-867-5309</p>
+        <p>Email: jenny@gmail.com</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
